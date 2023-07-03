a1b9748c038bdecac2f45585cda0c85575c416b8
Nikita Fast <N1kita.fast@yandex.ru>
2023-07-03 11:22:08 +0300

    updated readme

---
+++ build/CMakeFiles/yadro_task.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yadro_task.dir/src/awgn_channel.cpp.o"
+  "CMakeFiles/yadro_task.dir/src/awgn_channel.cpp.o.d"
+  "CMakeFiles/yadro_task.dir/src/main.cpp.o"
+  "CMakeFiles/yadro_task.dir/src/main.cpp.o.d"
+  "CMakeFiles/yadro_task.dir/src/qam_demodulator.cpp.o"
+  "CMakeFiles/yadro_task.dir/src/qam_demodulator.cpp.o.d"
+  "CMakeFiles/yadro_task.dir/src/qam_modulator.cpp.o"
+  "CMakeFiles/yadro_task.dir/src/qam_modulator.cpp.o.d"
+  "yadro_task"
+  "yadro_task.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yadro_task.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
